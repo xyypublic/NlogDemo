7733d9a2357389d27e6f881308921389187604ef
xyypublic <85662131+xyypublic@users.noreply.github.com>
2021-07-16 18:13:15 +0900

    first commit

---
+++ Common/Logger.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using System.IO;
+
+namespace Common
+{
+    public static class Logger
+    {
+        private static NLog.Logger loggerInfo = LogManager.GetLogger("info");
+        private static NLog.Logger loggerError = LogManager.GetLogger("error");
+        private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        /// info
+        /// </summary>
+        /// <param name="methodName"></param>
+        public static void Start(string methodName)
+        {
+            loggerInfo.Info($"[{methodName}]Start");
+        }
+
+        /// <summary>
+        /// error
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Error(string message)
+        {
+            loggerError.Error(message);
+        }
+
+        /// <summary>
+        /// debug
+        /// </summary>
+        /// <param name="message"></param>
+        public static void Common(string message)
+        {
+            logger.Debug(message);
+        }
+    }
+}
+
+
---
+++ Run/Program.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using System;
+
+namespace RunLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Common("test");
+        }
+    }
+}
